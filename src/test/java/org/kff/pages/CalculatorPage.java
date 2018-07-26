74b84664a92a6af1343cc4db799bbbaec1a7cab1
archimedesphysics <guvenyilmaz@gmail.com>
2018-07-26 15:15:05 -0500

    - calculate page first 4 information field locators entered

--- src/test/java/org/kff/pages/CalculatorPage.java
+++ src/test/java/org/kff/pages/CalculatorPage.java
@@ -1,5 +1,59 @@
 package org.kff.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CalculatorPage {
 
+	
+	private WebDriver driver;
+
+	public CalculatorPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	} 
+	
+	
+	@FindBy(id="zip-prompt")
+	public WebElement stateText; 
+	
+	@FindBy(id="state-wrapper")
+	public WebElement stateDropDown; 
+	
+	@FindBy(css="p:contains(^Enter your zip code$)")  //xpath="//p[.='Enter your zip code']"
+	public WebElement zipcodeText; 
+	
+	@FindBy (name="zip")
+	public WebElement zipcodeBox;
+
+	@FindBy(css="p:contains(^Select county$)")  //xpath="//p[.='Select county']"
+	public WebElement countText; 
+	
+	@FindBy(id="locale-inner")
+	public WebElement countyDropDown; 
+	
+	@FindBy(css="p:contains(^Enter income as$)")  //xpath="//p[.='Enter income as']"
+	public WebElement incomeText; 
+	
+	@FindBy(name="income-type")
+	public WebElement incomeDropDown; 
+	
+	@FindBy(id="income-prompt")
+	public WebElement householdText; 
+	
+	@FindBy(name="income")
+	public WebElement incomeBox; 
+	
+	@FindBy(className="prompt-with-tip")
+	public WebElement coverageavailableText; 
+	
+	@FindBy(name="employer-coverage")
+	public WebElement coverageDropDown; 
+	
+	
+	
+	
+	
 }
