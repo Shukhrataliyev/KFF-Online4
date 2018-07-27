1f828f96ceb5263caa07a78dc9ec58932b00cf55
acakici <acakici72@gmail.com>
2018-07-26 23:11:29 -0500

    pageData

--- src/test/java/org/kff/pages/StateDataPage.java
+++ src/test/java/org/kff/pages/StateDataPage.java
@@ -1,5 +1,28 @@
 package org.kff.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class StateDataPage {
+
+	private WebDriver driver;
+	public StateDataPage(WebDriver driver) {
+		
+		this.driver=driver;
+		
+PageFactory.initElements(driver, this);
+
+	
+	}
+	
+@FindBy(id="search-field" )	
+public WebElement searchField;
+
+@FindBy(name="geo-picker" )	
+public WebElement selectState;
+
+
 
 }
