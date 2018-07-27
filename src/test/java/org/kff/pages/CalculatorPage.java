d834a75aaa99e81310ecd653812fa809342bb1bb
ShukhratAliyev <shukhrataliyev@gmail.com>
2018-07-26 23:48:52 -0500

    test

--- src/test/java/org/kff/pages/CalculatorPage.java
+++ src/test/java/org/kff/pages/CalculatorPage.java
@@ -52,6 +52,9 @@ public class CalculatorPage {
 	@FindBy(name="employer-coverage")
 	public WebElement coverageDropDown; 
 	
+	@FindBy(name="employer-coverage")
+	public WebElement test; 
+	
 	
 	
 	
